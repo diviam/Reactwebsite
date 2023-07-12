d84197f3193c91fe478ba9af0c0102c85bd32eee
Rahulk3139 <122516989+Rahulk3139@users.noreply.github.com>
2023-07-12 15:45:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./pageComponent/Navbar";
+import Header from "./pageComponent/Header";
+import Main from "./pageComponent/Main";
+import Footer from "./pageComponent/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pageComponent/Main.js
@@ -0,0 +1,16 @@
+import About from '../pageComponent/About';
+import Services from '../pageComponent/Services';
+import Contact from '../pageComponent/Contact';
+
+function Main() {
+  return (
+    <>
+      <main>
+        <Services />
+        <About />
+        <Contact />
+      </main>
+    </>
+  );
+}
+export default Main;
